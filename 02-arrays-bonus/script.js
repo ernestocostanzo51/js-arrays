cd794884267713375753f774a5ecbaa340d672d1
ernestocostanzo51 <ernestanzo51@gmail.com>
2026-02-04 17:13:19 +0100

    utilizzo ciclo for nel primo esercizio

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -12,7 +12,11 @@ const teachers = [
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
 
-const reversedTeachers = 
+const reversedTeachers= []
+for(let i=teachers.length - 1; i>0; i--){
+  reversedTeachers.push(teachers[i])
+}
+console.log(reversedTeachers)
 
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
@@ -27,12 +31,17 @@ for(let i=0; i<teachers.length; i++){
 console.log(longNames)
 
 // 3. Rimuovi 'Ed' dall'array teachers
-teachers.splice('Ed')
+teachers.splice(teachers[1])
 console.log(teachers)
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+const isFabioPresent = 'fabio è presente';
+for(i=0; i<teachers.length; i++)
+  if(teachers[i] === 'Fabio'){
+    console.log(isFabioPresent)
+    break
+  }
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = null;
